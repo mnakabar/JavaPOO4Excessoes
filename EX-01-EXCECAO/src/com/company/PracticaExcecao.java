4766d3bfba309338104aa02df7c5720acb2d16d2
Mauricio <mauricio.nakabar@mercadolivre.com>
2021-08-26 20:40:00 -0300

    Excecoes

---
+++ EX-01-EXCECAO/src/com/company/PracticaExcecao.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class PracticaExcecao {
+    public int a = 0;
+    public int b = 300;
+
+    public PracticaExcecao(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+    public  void quociente(){
+        try {
+            double conta = this.b / this.a;
+            System.out.println("O resultado da conta: " + conta);
+        }
+        catch (Exception ex){
+            throw new IllegalArgumentException("Nao Pode ser dividio por zero");
+
+        }
+        //catch (Exception ex){
+        //    System.out.println("Ocorreu um erro");
+        //}
+
+    }
+
+
+
+}
+
